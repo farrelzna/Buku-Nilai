b8b71919204b764091dff9a56861b6dfbd43dfad
farrelzna <izzma853@gmail.com>
2025-02-17 11:10:52 +0700

    First Commit

---
+++ src/components/login/Form.vue
@@ -0,0 +1,55 @@
+<template>
+    <form @submit.prevent="proccesForm" class="card d-block mx-auto my-5 p-5 bg-light w-75 d-block m-auto">
+    <div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Username</label>
+            <input type="username" class="form-control" id="username" placeholder="Enter username" v-model="username" @change="detectlenghtvalue">
+            <small class="text-danger">{{ errorMessage.username }}</small>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Enter password" v-model="password">
+            <small class="text-danger">{{ errorMessage.password }}</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>  
+    </div>
+</form>
+</template>
+
+<script>
+export default {
+
+    name: 'FormLogin', //identitas form
+    data(){
+        return{
+            username: '',
+            password: '',
+            errorMessage : {
+                username: '',
+                password: '',
+            }
+        }
+    },
+    methods: {
+        detectlenghtvalue(){
+            if(this.username.length < 4){
+                this.errorMessage.username = 'Username minimal 4 karakter';
+            }else{
+                this.errorMessage.username = '';
+            }
+        },
+        proccesForm(){
+            if(!this.username){
+                this.errorMessage.username = 
+                'Username tidak boleh kosong';
+            }if(!this.password){
+                this.errorMessage.password = 
+                'Password tidak boleh kosong';
+            }if(this.username && this.password){
+                alert('Sedang Memproses Data');
+            }
+        }
+    },
+
+}
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <FormLogin></FormLogin>
+</template>
+
+<script>
+import FormLogin from '../components/login/Form.vue'
+export default {
+    name: "LoginPage", 
+    //Identitas Component // mendaftarkan component yang akan digunakan di halaman ini, isisnya sama dengan nama di import
+    components: {
+        FormLogin
+    }
+}
+</script>
