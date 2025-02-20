326ca8a2f8937699819529e860e3403641aabc59
farrelzna <izzma853@gmail.com>
2025-02-20 17:21:51 +0700

    Update > 02/20/2025

--- src/components/login/Form.vue
+++ src/components/login/Form.vue
@@ -1,55 +1,87 @@
 <template>
     <form @submit.prevent="proccesForm" class="card d-block mx-auto my-5 p-5 bg-light w-75 d-block m-auto">
-    <div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Username</label>
-            <input type="username" class="form-control" id="username" placeholder="Enter username" v-model="username" @change="detectlenghtvalue">
-            <small class="text-danger">{{ errorMessage.username }}</small>
+        <div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Username</label>
+                <input type="username" class="form-control" id="username" placeholder="Enter username"
+                    v-model="username" @change="detectlenghtvalue">
+                <small class="text-danger">{{ errorMessage.username }}</small>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter password"
+                    v-model="password">
+                <small class="text-danger">{{ errorMessage.password }}</small>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Enter password" v-model="password">
-            <small class="text-danger">{{ errorMessage.password }}</small>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>  
-    </div>
-</form>
+    </form>
 </template>
 
 <script>
+import axios from 'axios';
+import { userRouter } from 'vue-router';
+const router = userRouter();
+
 export default {
 
     name: 'FormLogin', //identitas form
-    data(){
-        return{
+    data() {
+        return {
             username: '',
             password: '',
-            errorMessage : {
+            errorMessage: {
                 username: '',
                 password: '',
             }
         }
     },
     methods: {
-        detectlenghtvalue(){
-            if(this.username.length < 4){
+        detectlenghtvalue() {
+            if (this.username.length < 4) {
                 this.errorMessage.username = 'Username minimal 4 karakter';
-            }else{
+            } else {
                 this.errorMessage.username = '';
             }
         },
-        proccesForm(){
-            if(!this.username){
-                this.errorMessage.username = 
-                'Username tidak boleh kosong';
-            }if(!this.password){
-                this.errorMessage.password = 
-                'Password tidak boleh kosong';
-            }if(this.username && this.password){
-                alert('Sedang Memproses Data');
+        proccesForm() {
+            if (!this.username) {
+                this.errorMessage.username =
+                    'Username tidak boleh kosong';
             }
+            if (!this.password) {
+                this.errorMessage.password =
+                    'Password tidak boleh kosong';
+            }
+            if (this.username && this.password) {
+                // siapkan data yang akan dicek
+                let payload = {
+                    username: this.username,
+                    password: this.password
+                }
+                this.hitAxios('https://localhost:3000/users', payload);
+            }
+        },
+        hitAxios(endpoint, payload) {
+            axios({
+                method: 'get',
+                url: endpoint,
+                responseType: 'json'
+            })
+            .then(function (response) {
+                // console.log(response);
+                let users = response.data;
+                let userByUsername = users.filter(function(user) {
+                    return user.username == payload.username && user.password == payload.password;
+                })
+                if (userByUsername.length > 0) {
+                    // alert('Anda Berhasil Login}')
+                    router.push('/dashboard');
+                } else {
+                    alert ('Anda Gagal Login, Try Again!');
+                }
+            });
         }
-    },
-
+    }
 }
 </script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/pages/Login.vue';
+import Dashboard from '@/pages/Dashboard.vue';
 
 const routes = [
     {
         path: '/login',
         name: 'Login',
         component: <Login />    
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: <Dashboard />
     }
 ]
 
